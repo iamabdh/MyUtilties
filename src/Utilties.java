5404a778942d24aafef4bc2e87795dee1e6fce43
iamabdh <abdullahalbadri09@gmail.com>
2022-12-14 09:38:50 +0400

    initial commit

---
+++ src/Utilties.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-4
+ *
+ */
+public class Utilties {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
