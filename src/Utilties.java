b9d799c057bda2a5ae7558725243e23ad5140c3f
iamabdh <abdullahalbadri09@gmail.com>
2022-12-18 12:23:31 +0400

    largest number in  array

--- src/Utilties.java
+++ src/Utilties.java
@@ -1,3 +1,10 @@
+import java.awt.im.InputMethodHighlight;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,8 +19,17 @@ public class Utilties {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("output: "+ findLargestNumberArray(new int[] {50, 12, 8, 10, 1001, 6, 87}));
+	}
+	
+	public static int findLargestNumberArray(int[] arr) {
+		int max = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
 	}
 
 }
