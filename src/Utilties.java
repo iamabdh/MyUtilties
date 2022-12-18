7ee72f49b99dea982b801ece8cc107d830b328b6
iamabdh <abdullahalbadri09@gmail.com>
2022-12-18 13:48:44 +0400

    second & third largest three loops

--- src/Utilties.java
+++ src/Utilties.java
@@ -1,5 +1,6 @@
 import java.awt.im.InputMethodHighlight;
 import java.lang.reflect.Array;
+import java.security.Identity;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,17 +20,36 @@ public class Utilties {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("output: "+ findLargestNumberArray(new int[] {50, 12, 8, 10, 1001, 6, 87}));
+		int[] arr = new int[] {1, 12, 8, 400,  10, 1001, 6, 87, 300};
+		int[] output = findLargestNumberArray(arr);
+		System.out.println("max: "+ output[0]);
+		System.out.println("second: "+ output[1]);
+		System.out.println("third: "+ output[2]);
 	}
 	
-	public static int findLargestNumberArray(int[] arr) {
+	public static int[] findLargestNumberArray(int[] arr) {
 		int max = arr[0];
+		int secondMax =  arr[0];
+		int thirdMax = arr[0];
 		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] > max) {
+			if (max < arr[i]) {
 				max = arr[i];
 			}
 		}
-		return max;
+		
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < max && secondMax < arr[i]) {
+				secondMax = arr[i];
+			}
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < max && arr[i] < secondMax && thirdMax < arr[i]) {
+				thirdMax = arr[i];
+			}
+		}
+		
+		return new int[] {max, secondMax, thirdMax};
 	}
 
 }
